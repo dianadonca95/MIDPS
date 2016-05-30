ea7aa3fe71ebf7f06eb3671c16d389b30857d8fe
Diana <Diana>
2016-05-30 10:33:34 +0300

    LABS

---
+++ lab4/Lab4MIDPS/index.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Tyrannosaurus</title>
+	<link rel="stylesheet" type="text/css" href="style/main.css">
+</head>
+<body>
+		<h1>Dangerous Dinosaur</h1>
+		<div id="feed">
+			<a href="feed.php">Feedback</a>
+		</div>
+		<p id="line"></p>
+		<ul id="menu">
+			<li><a href="#"> Home</a></li>
+			<li><a href="page2.php">About</a></li>
+			<li><a href="page3.php">Wallpapers</a></li>
+		</ul>
+		<p id="line"></p>
+
+		<h2>Tyrannosaurus Rex</h2>
+
+		<img src="./img/ttt/1t.jpg"Owl img" align="center">
+		<p class="order">
+		<br>
+			Tyrannosaurus is a genus of coelurosaurian theropod dinosaur. The species Tyrannosaurus rex (rex meaning "king" in Latin), is one of the most well-represented of the large theropods. Tyrannosaurus lived throughout what is now western North America, on what was then an island continent known as Laramidia. Tyrannosaurus had a much wider range than other tyrannosaurids. Fossils are found in a variety of rock formations dating to the Maastrichtian age of the upper Cretaceous Period, 68 to 66 million years ago. It was the last known member of the tyrannosaurids, and among the last non-avian dinosaurs to exist before the Cretaceous-Paleogene extinction event."
+			<br><br>
+			Like other tyrannosaurids, Tyrannosaurus was a bipedal carnivore with a massive skull balanced by a long, heavy tail. Relative to its large and powerful hind limbs, Tyrannosaurus fore limbs were short but unusually powerful for their size and had two clawed digits. The most complete specimen measures up to 12.3 m (40 ft) in length, up to 4 meters (13 ft) tall at the hips, and up to 9.5 metric tons (10.5 short tons) in weight. Although other theropods rivaled or exceeded Tyrannosaurus rex in size, it is still among the largest known land predators and is estimated to have exerted the largest bite force among all terrestrial animals. By far the largest carnivore in its environment, Tyrannosaurus rex was most likely an apex predator, preying upon hadrosaurs, ceratopsians, and possibly sauropods. Some experts, however, have suggested the dinosaur was primarily a scavenger. The question of whether Tyrannosaurus was an apex predator or a pure scavenger was among the longest ongoing debates in paleontology. It is accepted now that Tyrannosaurus rex acted as a predator, and scavenged as modern mammalian and avian predators do. <br><br>
+		More than 50 specimens of Tyrannosaurus rex have been identified, some of which are nearly complete skeletons. Soft tissue and proteins have been reported in at least one of these specimens. The abundance of fossil material has allowed significant research into many aspects of its biology, including its life history and biomechanics. The feeding habits, physiology and potential speed of Tyrannosaurus rex are a few subjects of debate. Its taxonomy is also controversial, as some scientists consider Tarbosaurus bataar from Asia to be a second Tyrannosaurus species while others maintain Tarbosaurus is a separate genus. Several other genera of North American tyrannosaurids have also been synonymized with Tyrannosaurus.
+<br>
+		</p>
+	
+
+		<img src="./img/ttt/2t.jpg">
+		<br>
+		<p class="order">
+			Tyrannosaurus rex was one of the largest land carnivores of all time; the largest complete specimen, located at the Field Museum of Natural History under the name FMNH PR2081 and nicknamed Sue, measured 12.3 meters (40 ft) long, and was 4 meters (13 ft) tall at the hips. Mass estimates have varied widely over the years, from more than 7.2 metric tons (7.9 short tons), to less than 4.5 metric tons (5.0 short tons),page needed] with most modern estimates ranging between 5.4 metric tons (6.0 short tons) and 6.8 metric tons (7.5 short tons). One study in 2011 found that the maximum weight of Sue, the largest Tyrannosaurus, was between 9.5 and 18.5 metric tons (9.3-18.2 long tons; 10.5-20.4 short tons), though the authors stated that their upper and lower estimates were based on models with wide error bars and that they "consider to be too skinny, too fat, or too disproportionate". Packard et al. (2009) tested dinosaur mass estimation procedures on elephants and concluded that those of dinosaurs are flawed and produce over-estimations; thus, the weight of Tyrannosaurus could have been much less than previously thought. Other estimations have concluded that the largest known Tyrannosaurus specimens had masses approaching or exceeding 9 tonnes.
+			<br><br>
+		The neck of Tyrannosaurus rex formed a natural S-shaped curve like that of other theropods, but was short and muscular to support the massive head. The forelimbs had only two clawed fingers, along with an additional small metacarpal representing the remnant of a third digit. In contrast the hind limbs were among the longest in proportion to body size of any theropod. The tail was heavy and long, sometimes containing over forty vertebrae, in order to balance the massive head and torso. To compensate for the immense bulk of the animal, many bones throughout the skeleton were hollow, reducing its weight without significant loss of strength.
+			<br><br>
+The largest known Tyrannosaurus rex skulls measure up to 1.5 meters (4.9 ft) in length. Large fenestrae (openings) in the skull reduced weight and provided areas for muscle attachment, as in all carnivorous theropods. But in other respects Tyrannosaurus's skull was significantly different from those of large non-tyrannosauroid theropods. It was extremely wide at the rear but had a narrow snout, allowing unusually good binocular vision. The skull bones were massive and the nasals and some other bones were fused, preventing movement between them; but many were pneumatized (contained a "honeycomb" of tiny air spaces) which may have made the bones more flexible as well as lighter. These and other skull-strengthening features are part of the tyrannosaurid trend towards an increasingly powerful bite, which easily surpassed that of all non-tyrannosaurids. The tip of the upper jaw was U-shaped (most non-tyrannosauroid carnivores had V-shaped upper jaws), which increased the amount of tissue and bone a tyrannosaur could rip out with one bite, although it also increased the stresses on the front teeth
+			<br><br>
+	
+		</p>
+	
+		<img src="./img/ttt/3t.jpg" alt="No Internet connection!">
+		<br>
+		<p class="order">
+		The teeth of Tyrannosaurus rex displayed marked heterodonty (differences in shape). The premaxillary teeth at the front of the upper jaw were closely packed, D-shaped in cross-section, had reinforcing ridges on the rear surface, were incisiform (their tips were chisel-like blades) and curved backwards. The D-shaped cross-section, reinforcing ridges and backwards curve reduced the risk that the teeth would snap when Tyrannosaurus bit and pulled. The remaining teeth were robust, like "lethal bananas" rather than daggers, more widely spaced and also had reinforcing ridges. Those in the upper jaw were larger than those in all but the rear of the lower jaw. The largest found so far is estimated to have been 30 centimeters (12 in) long including the root when the animal was alive, making it the largest tooth of any carnivorous dinosaur yet found.<br><br>
+
+			While there is no direct evidence for Tyrannosaurus rex having had feathers, many scientists now consider it likely that T. rex had feathers on at least parts of its body, due to their presence in related species of similar size. Mark Norell of the American Museum of Natural History summarized the balance of evidence by stating that: "we have as much evidence that T. rex was feathered, at least during some stage of its life, as we do that australopithecines like Lucy had hair."	
+			<br><br>
+
+		</p>
+		<?php
+			$connection = new mysqli("localhost", "root", "");
+			mysqli_select_db($connection, "article");
+			$result = $connection->query("SELECT * FROM title"); 
+			if($result->num_rows){
+				while($row= $result->fetch_assoc()) {
+					echo $row["full_text"];
+					echo $row["title"];
+				}
+			}
+		?>
+		<footer>
+			<div id="rights">
+				<div>All rights reserved &copy; <?= date ('Y')?> </div>
+			</div>
+		</footer>
+	</body>
+</html>
